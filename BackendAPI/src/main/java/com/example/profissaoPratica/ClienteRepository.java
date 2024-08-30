b8a3bee79f8bb6099e2d88fbfd8ac169199480c5
V1ctorNunes <victor.moreira@sptech.school>
2024-08-30 15:53:26 -0300

    correção ambiente qa

--- BackendAPI/src/main/java/com/example/profissaoPratica/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.profissaoPratica;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<ClienteEntity, Integer> {
-
-}
