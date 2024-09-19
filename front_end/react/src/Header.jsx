c73da48fdb84d7989d5e52366d9e5e576e7fdff5
carlosOsilva <carlos.osilva@sptech.school>
2024-09-19 10:23:16 -0300

    Arquivo criado e início do código front end em React e Typescript

---
+++ front_end/react/src/Header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+
+    return(
+        <header>
+            <h1>Sitezin</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Principal</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Serviços</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </nav>
+            <hr />
+        </header>
+    );
+}
+
+export default Header;
